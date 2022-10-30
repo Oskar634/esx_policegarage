046b6bd71409905347867ecae54db87fd144fbea
Copy634 <90392589+copy634@users.noreply.github.com>
2022-10-30 21:03:32 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,111 @@
+local isDead = false
+local inVehicle = false
+
+ESX = nil
+
+Citizen.CreateThread(
+    function()
+        while ESX == nil do
+            TriggerEvent("esx:getSharedObject", function(obj)
+            ESX = obj
+            end)
+        Citizen.Wait(0)
+    end
+    while ESX.GetPlayerData().job == nil do
+        Citizen.Wait(10)
+    end
+    PlayerData = ESX.GetPlayerData()
+end)
+
+RegisterNetEvent("esx:playerLoaded")
+AddEventHandler("esx:playerLoaded", function(xPlayer)
+    ESX.PlayerData = xPlayer
+end)
+
+RegisterNetEvent("esx:setJob")
+AddEventHandler("esx:setJob", function(job)
+    ESX.PlayerData.job = job
+end)
+
+CreateThread(function()
+    while true do
+        local wait = 1000
+        local ped = PlayerPedId()
+        local pcoords = GetEntityCoords(ped)
+        for k, v in pairs(Config.Tallit) do
+            if #(pcoords - v.Spawn) < 10 then
+				wait = 0
+                if ESX.PlayerData.job.name == "police" and not isDead then
+					DrawMarker(1, v.Spawn.x, v.Spawn.y, v.Spawn.z - 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 0.5, 255, 255, 255, 0.05, 0, 0, 0, 0, 0, 0, 0)
+                    if #(pcoords - v.Spawn) < 1.0 then
+                        ESX.ShowHelpNotification("~INPUT_PICKUP~ Avaa autotalli")
+                        if IsControlJustReleased(0, 38) then
+                            local spawncoords = v.SpawnCoordit
+                            AvaaTalli(spawncoords)
+                        end
+                    end
+                end
+            end
+        end
+		Wait(wait)
+    end
+end)
+
+CreateThread(function()
+    while true do
+        local wait = 1000
+        local ped = PlayerPedId()
+        local pcoords = GetEntityCoords(ped)
+        for k, v in pairs(Config.Tallit) do
+            if #(pcoords - v.Poista) < 10 then
+                if not isDead and inVehicle then
+					wait = 0
+                    DrawMarker(1, v.Poista.x, v.Poista.y, v.Poista.z - 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 0.5, 255, 255, 255, 0.05, 0, 0, 0, 0, 0, 0, 0)
+                    if #(pcoords - v.Poista) < 1.0 and ESX.PlayerData.job.name == "police" then
+                        ESX.ShowHelpNotification("~INPUT_PICKUP~ Poista ajoneuvo")
+                        if IsControlJustReleased(0, 38) then
+                            PoistaAjoneuvo()
+                        end
+                    end
+                end
+            end
+        end
+		Wait(wait)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        local ped = PlayerPedId()
+        isDead = IsPedDeadOrDying(ped)
+        inVehicle = IsPedInAnyVehicle(ped, true)
+        Citizen.Wait(500)
+    end
+end)
+
+function PoistaAjoneuvo()
+    local ped = PlayerPedId()
+    local vehicle = GetVehiclePedIsIn(ped)
+    ESX.Game.DeleteVehicle(vehicle)
+end
+
+function AvaaTalli(spawncoords)
+    ESX.UI.Menu.CloseAll()
+
+    ESX.UI.Menu.Open("default", GetCurrentResourceName(), "autotallimenu",
+        {
+            title = "Poliisi talli",
+            align = "top-right",
+            elements = Config.Ajoneuvot
+        },
+        function(data, menu)
+            ESX.UI.Menu.CloseAll()
+            local model = data.current.value
+            ESX.Game.SpawnVehicle(model, spawncoords, 0, function(vehicle) end)
+        end,
+        function(data, menu)
+            menu.close()
+        end,
+        function(data, menu)
+        end)
+end
---
+++ config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+Config.Tallit = {
+    [1] = {
+        Spawn = vec3(458.72, -1017.12, 28.1),
+        Poista = vec3(449.52, -1017.90, 28.52),
+        SpawnCoordit = vec4(445.92, -1024.45, 28.52, 107)
+    },
+
+    [2] = {
+        Spawn = vec3(1864.85, 3690.6, 34.26),
+        Poista = vec3(1869.27, 3694.1, 33.59),
+        SpawnCoordit = vec3(1869.27, 3694.1, 33.59)
+    }
+}
+
+Config.Ajoneuvot = {
+    {label = 'Police 1', value = 'police'},
+    {label = 'Police 2', value = 'police2'},
+}
