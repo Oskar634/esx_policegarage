cfda62c7ba2b0b5af9e011ac753740d9ba8dfa93
Copy634 <90392589+copy634@users.noreply.github.com>
2022-10-30 21:03:44 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Oskar#2222'
+description 'ESX Poliisi talli'
+version '1.0.0'
+
+client_scripts {
+	'@es_extended/imports.lua',
+	'config.lua',
+	'client.lua'
+}
