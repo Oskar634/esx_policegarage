d226c8dfff934bd37af1e349120409494835a9b8
Oskar634 <90392589+Oskar634@users.noreply.github.com>
2022-10-31 17:55:49 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,13 +4,15 @@ Config.Tallit = {
     [1] = {
         Spawn = vec3(458.72, -1017.12, 28.1),
         Poista = vec3(449.52, -1017.90, 28.52),
-        SpawnCoordit = vec3(445.92, -1024.45, 28.52)
+        SpawnCoordit = vec3(445.92, -1024.45, 28.52),
+        Heading = 70.0
     },
 
     [2] = {
         Spawn = vec3(1864.85, 3690.6, 34.26),
         Poista = vec3(1869.27, 3694.1, 33.59),
-        SpawnCoordit = vec3(1869.27, 3694.1, 33.59)
+        SpawnCoordit = vec3(1869.27, 3694.1, 33.59),
+        Heading = 70.0
     }
 }
 
