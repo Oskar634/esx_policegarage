6cb33f48adda4dceef9cf311dc7875349a97e794
Oskar634 <90392589+Oskar634@users.noreply.github.com>
2022-10-31 16:28:39 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -10,9 +10,9 @@ CreateThread(function()
         local pcoords = GetEntityCoords(ped)
         for k, v in pairs(Config.Tallit) do
             if #(pcoords - v.Spawn) < 10 then
-		wait = 0
+		    wait = 0
                 if ESX.PlayerData.job.name == "police" and not isDead then
-		    DrawMarker(1, v.Spawn.x, v.Spawn.y, v.Spawn.z - 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 0.5, 255, 255, 255, 0.05, 0, 0, 0, 0, 0, 0, 0)
+		        DrawMarker(1, v.Spawn.x, v.Spawn.y, v.Spawn.z - 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 0.5, 255, 255, 255, 0.05, 0, 0, 0, 0, 0, 0, 0)
                     if #(pcoords - v.Spawn) < 1.0 then
                         ESX.ShowHelpNotification("~INPUT_PICKUP~ Avaa autotalli")
                         if IsControlJustReleased(0, 38) then
@@ -35,8 +35,8 @@ CreateThread(function()
         for k, v in pairs(Config.Tallit) do
             if #(pcoords - v.Poista) < 10 and ESX.PlayerData.job.name == "police" then
                 if not isDead and inVehicle then
-		    wait = 0
-                    DrawMarker(1, v.Poista.x, v.Poista.y, v.Poista.z - 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 0.5, 255, 255, 255, 0.05, 0, 0, 0, 0, 0, 0, 0)
+		        wait = 0
+                DrawMarker(1, v.Poista.x, v.Poista.y, v.Poista.z - 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 0.5, 255, 255, 255, 0.05, 0, 0, 0, 0, 0, 0, 0)
                     if #(pcoords - v.Poista) < 1.0 then
                         ESX.ShowHelpNotification("~INPUT_PICKUP~ Poista ajoneuvo")
                         if IsControlJustReleased(0, 38) then
