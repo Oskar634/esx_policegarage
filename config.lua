7dd3493999fe00ac86796b6be600fc774aabd88f
Oskar634 <90392589+Oskar634@users.noreply.github.com>
2023-05-08 15:45:01 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,22 +1,22 @@
 Config = {}
 
-Config.Tallit = {
+Config.Garages = {
     [1] = {
         Spawn = vec3(458.72, -1017.12, 28.1),
-        Poista = vec3(449.52, -1017.90, 28.52),
-        SpawnCoordit = vec3(445.92, -1024.45, 28.52),
+        Remove = vec3(449.52, -1017.90, 28.52),
+        SpawnCoords = vec3(445.92, -1024.45, 28.52),
         Heading = 70.0
     },
 
     [2] = {
         Spawn = vec3(1864.85, 3690.6, 34.26),
-        Poista = vec3(1869.27, 3694.1, 33.59),
-        SpawnCoordit = vec3(1869.27, 3694.1, 33.59),
+        Remove = vec3(1869.27, 3694.1, 33.59),
+        SpawnCoords = vec3(1869.27, 3694.1, 33.59),
         Heading = 70.0
     }
 }
 
-Config.Ajoneuvot = {
+Config.Vehicles = {
     {label = 'Police 1', value = 'police'},
     {label = 'Police 2', value = 'police2'},
 }
